090f2cb54d432c616c6dd20091db7d957094632f
ShiyuuShimo <shiyuu.shimomura@gmail.com>
2024-05-24 22:47:27 +0900

    test for JS and CSS for color of links

---
+++ src/header.js
@@ -0,0 +1,8 @@
+document.write(
+	'<meta charset="utf-8">' +
+	'<meta name="description" content="homepage of sf-promnade">' +
+	'<meta name="author" content="ShiyuuShimo">' +
+	'<meta name="keyword" content="science fiction">' +
+	'<base href="https://shiyuushimo.github.io/sf-promenade.github.io/">' +
+	'<link rel="stylesheet" type="text/css" href="css/style.css">'
+);
