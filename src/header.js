2575bcc9b401963fea5830492a0c8cb605d7f48d
ShiyuuShimo <shiyuu.shimomura@gmail.com>
2024-06-23 20:44:55 +0900

    add MathJax

--- src/header.js
+++ src/header.js
@@ -5,5 +5,6 @@ document.write(
 	'<meta name="keyword" content="science fiction">' +
 	'<base href="https://shiyuushimo.github.io/sf-promenade.github.io/">' +
 	'<link rel="stylesheet" type="text/css" href="css/style.css">' +
-	'<link rel="stylesheet" type="text/css" href="css/link.css">'
+	'<link rel="stylesheet" type="text/css" href="css/link.css">' +
+	'<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>'
 );
